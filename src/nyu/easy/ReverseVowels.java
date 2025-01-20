f2b282976988fe135612054412ffa1ff6d7e9d6e
Blessed1 <shtoshtemirov@cdti.uz>
2025-01-20 19:13:23 +0500

    nimadir boldi llkn ishlamadi swap vowel

--- src/nyu/easy/ReverseVowels.java
+++ src/nyu/easy/ReverseVowels.java
@@ -1,9 +1,6 @@
 package nyu.easy;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /**
  * <h>345. Reverse Vowels of a String
@@ -16,17 +13,40 @@ import java.util.Set;
  * */
 public class ReverseVowels {
     public String reverseVowels(String s) {
-        int begin= 0; int end= s.length();
+        int begin= 0; int end= s.length()-1;
+        char[] c = s.toCharArray();
         Set<Character> set=new HashSet<Character>(){};
-        set.addAll(List.of('A','E','I','O','U'));
+        set.addAll(List.of('A','E','I','O','U','a','e','i','o','u'));
+        int tempb =-1, tempe=-1;
+        char tempp = 0, temppdd = 0;
         while (begin<end) {
-            int tempb =-1, tempe=-1;
-            if (set.contains(s.charAt(begin))){
+
+            if (set.contains(c[begin])){
+                tempp = c[begin];
+                tempb = begin;
             }
 
-            if (set.contains(s.charAt(end))){
-
+            if (set.contains(c[end])){
+                tempe = end;
+                temppdd = c[end];
             }
+            if (tempb!=-1 && tempe!=-1){
+                c[tempb] = temppdd;
+                c[tempe] = tempp;
+                tempb = -1;
+                tempe = -1;
+            }
+            begin++;
+            end--;
         }
+        return String.copyValueOf(c);
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new ReverseVowels().reverseVowels("Euston saw I was not Sue."));
     }
 }
+//euston saw I was not SuE.
+
+//Eestun sow a wIs nat Sou.
+//Euston saw I was not Sue.
